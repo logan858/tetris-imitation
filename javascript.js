ac2a50282e0b4b638cd10f23f88fe5e967448ead
Logan <mlogan858@hotmail.com>
2021-02-21 10:14:37 -0500

    simplifying x-y axis trackers

--- javascript.js
+++ javascript.js
@@ -48,23 +48,38 @@ cGrid(200);
 
 //VARS
 //starting positions for each block on the x axis
-let square1 = 5
-let square2 = 4
-let counter = 0
+let xAxis = 4;
+let yAxis = 0;
+let = displayXAxis = 5;
+let boardYAxis = 0;
+let counter = 0;
 let scoreTotal = 0;
 let currentShape = 0;
 //variables to hold dom objects 
 function smashBoy(i) {
-    tetrisBoard[0 + i][square1] = 1
-    tetrisBoard[0 + i][square2]  = 1
-    tetrisBoard[1 + i][square1] = 1
-    tetrisBoard[1 + i][square2] = 1
+    tetrisBoard[i][xAxis] = 1
+    tetrisBoard[i + 1][xAxis] = 1
+    tetrisBoard[i][xAxis + 1]  = 1
+    tetrisBoard[i + 1][xAxis + 1] = 1
 }
 function orangeRicky(i) {
-    tetrisBoard[0 + i][square2] = 1
-    tetrisBoard[1 + i][square2]  = 1
-    tetrisBoard[2 + i][square2] = 1
-    tetrisBoard[2 + i][square1] = 1
+    tetrisBoard[i][xAxis] = 1
+    tetrisBoard[i + 1][xAxis]  = 1
+    tetrisBoard[i + 2][xAxis] = 1
+    tetrisBoard[i + 2][xAxis + 1] = 1
+}
+function boardStateEraser(i) {
+    if(currentShape == 1) {
+        tetrisBoard[i - 1][xAxis] = 0
+        tetrisBoard[i][xAxis] = 0
+        tetrisBoard[i - 1][xAxis + 1]  = 0
+        tetrisBoard[i][xAxis + 1] = 0
+    } else if(currentShape == 2) {
+        tetrisBoard[i - 1][xAxis] = 1
+        tetrisBoard[i][xAxis]  = 1
+        tetrisBoard[i + 1][xAxis] = 1
+        tetrisBoard[i + 1][xAxis + 1] = 1
+    }
 }
 
 
@@ -79,43 +94,43 @@ function scoreTracker(x) {
 
 function smashBoyShaper() {
     currentShape = 1;
+    counter = 0;
     let i = 0;
     let z = setInterval(function() {
         if(i >= 1) {
             shapeErase(i)
-            for(j = 0; j < tetrisBoard.length; j++) {
-                tetrisBoard[i - 1][j] = 0;
-            }
+            boardStateEraser(i)
         }
         smashBoy(i)
         i += 1;
         counter += 1;
         smashBoyDisplay(i)
+        console.log(tetrisBoard)
         if (i + 1 >= tetrisBoard.length) {
             clearInterval(z);
+            currentShape = 0;
             return;
         }
     }, 100)   
 }
+
 function orangeRickyShaper() {
     currentShape = 2;
+    counter = 0;
     let i = 0;
     let z = setInterval(function() {
         if(i >= 1) {
             shapeErase(i)
-            tetrisBoard[i - 1][square2] = 0
-            tetrisBoard[i][square1] = 0
-            tetrisBoard[i - 1][square1] = 0
+            boardStateEraser(i)
         }
-
         orangeRicky(i)
         i += 1;
         counter += 1;
         orangeRickyDisplay(i)
+        console.log(tetrisBoard)
         if (i + 2 >= tetrisBoard.length) {
             clearInterval(z);
-            console.log(tetrisBoard)
-            counter = 0;
+            currentShape = 0;
             return;
         }
     }, 100)   
@@ -125,42 +140,38 @@ function orangeRickyShaper() {
 
 //Visual functions that add visual css classlists, & remove them
 function smashBoyDisplay(x) {
-    let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square1 + 1] + ")") 
-    let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square2 + 1] + ")") 
-    let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square1 + 1] + ")") 
-    let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square2 + 1] + ")") 
-    blockLight.classList.add("blockpiece")
-    blockLight2.classList.add("blockpiece")
-    blockLight3.classList.add("blockpiece")
-    blockLight4.classList.add("blockpiece")
+    let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis] + ")") 
+    let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis + 1] + ")") 
+    let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis] + ")") 
+    let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis + 1] + ")") 
+    let smashBoy = [blockLight, blockLight2, blockLight3, blockLight4]
+    smashBoy.forEach(ele => ele.classList.add("blockpiece"))
 }
 function orangeRickyDisplay(x) {
-    let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square2 + 1] + ")") 
-    let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square2 + 1] + ")") 
-    let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + square2 + 1] + ")") 
-    let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + square1+ 1] + ")") 
-    blockLight.classList.add("blockpiece")
-    blockLight2.classList.add("blockpiece")
-    blockLight3.classList.add("blockpiece")
-    blockLight4.classList.add("blockpiece")
+    let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis] + ")") 
+    let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis] + ")") 
+    let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + displayXAxis] + ")") 
+    let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + displayXAxis + 1] + ")") 
+    let orangeRicky = [blockLight, blockLight2, blockLight3, blockLight4]
+    orangeRicky.forEach(ele => ele.classList.add("blockpiece"))
 }
 
 // shapeErase will check to see which shape function is stored in a changing variable, and execute an according if eraser
 function shapeErase(x) {
     if(currentShape == 1) {
-        let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square1 + 1] + ")") 
-        let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square2 + 1] + ")") 
-        let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square1 + 1] + ")") 
-        let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square2 + 1] + ")")
+        let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis] + ")") 
+        let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis + 1] + ")") 
+        let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis] + ")") 
+        let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis + 1] + ")")
         blockLight.classList.remove("blockpiece")
         blockLight2.classList.remove("blockpiece")
         blockLight3.classList.remove("blockpiece")
         blockLight4.classList.remove("blockpiece") 
     } else if (currentShape == 2) {
-        let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + square2 + 1] + ")") 
-        let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + square2 + 1] + ")") 
-        let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + square2 + 1] + ")") 
-        let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + square1+ 1] + ")") 
+        let blockLight = document.querySelector("#boardplate div:nth-child(" + [[x - 1] * 10 + displayXAxis] + ")") 
+        let blockLight2 = document.querySelector("#boardplate div:nth-child(" + [x * 10 + displayXAxis] + ")") 
+        let blockLight3 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + displayXAxis] + ")") 
+        let blockLight4 = document.querySelector("#boardplate div:nth-child(" + [[x + 1] * 10 + displayXAxis + 1] + ")") 
         blockLight.classList.remove("blockpiece")
         blockLight2.classList.remove("blockpiece")
         blockLight3.classList.remove("blockpiece")
@@ -179,30 +190,22 @@ function shapeErase(x) {
 //will have to also refuse inputs when counter = 0, when the function has an interval
 document.addEventListener("keydown", function(evnt) {
     if(evnt.code == "ArrowLeft") {
-        if (square1 > 1) {
-            if (counter + 1 < tetrisBoard.length) {
+        if (xAxis > 0) {
+            if ([counter + 1] < tetrisBoard.length) {
                 shapeErase(counter)
+                boardStateEraser(counter)
             }
-            square1 -= 1;
+            xAxis -= 1;
+            displayXAxis -=1;
         } 
-        if (square2 > 0) {
-            if (counter + 1 < tetrisBoard.length) {
-                shapeErase(counter)
-            }
-            square2 -= 1;
-        }
     } else if (evnt.code == "ArrowRight") {
-        if(square1 < 9) {
-            if (counter + 1 < tetrisBoard.length) {
+        if(xAxis < 8) {
+            if ([counter + 1] < tetrisBoard.length) {
                 shapeErase(counter)
+                boardStateEraser(counter)
             }
-            square1 += 1;
-        }
-        if(square2 < 8) {
-            if (counter + 1 < tetrisBoard.length) {
-                shapeErase(counter)
-            }
-            square2 += 1;
+            xAxis += 1;
+            displayXAxis += 1;
         }
     } 
 })
